135e49012f0ca951d488034d750a47a4267e7606
Alexander <akcanadanet@gmail.com>
2020-06-07 14:34:14 -0400

    Screen wrapping with bounding boxes

--- player.py
+++ player.py
@@ -14,7 +14,30 @@ class Player:
 			Vector2(-0.1, -0.1),
 			Vector2(0.1, 0),
 		]
-	
+
+	@property
+	def boundingBox(self):
+		minX = self.shape[0].x
+		minY = self.shape[0].y
+		maxX = self.shape[0].x
+		maxY = self.shape[0].y
+		for vertex in self.shape:
+			if vertex.x < minX: minX = vertex.x
+			if vertex.x > maxX: maxX = vertex.x
+			if vertex.y < minY: minY = vertex.y
+			if vertex.y > maxY: maxY = vertex.y
+		return (minX, minY), (maxX, maxY)
+
+	def screenWrap(self):
+		boundingBox = self.boundingBox
+		playerWidth = (boundingBox[1][0] - boundingBox[0][0]) / 2
+		playerHeight = (boundingBox[1][1] - boundingBox[0][1]) / 2
+
+		if self.position.x >  1 + playerWidth:  self.position.x = -1 - playerWidth
+		if self.position.x < -1 - playerWidth:  self.position.x =  1 + playerWidth
+		if self.position.y >  1 + playerHeight: self.position.y = -1 - playerHeight
+		if self.position.y < -1 - playerHeight: self.position.y =  1 + playerHeight
+		
 	def draw(self, canvas):
 		draw(canvas, self.shape, self.position, self.angle)
 
@@ -22,8 +45,4 @@ class Player:
 		self.draw(canvas)
 		velocity = Vector2(cos(radians(self.angle)) * self.speed, sin(radians(self.angle)) * self.speed)
 		self.position += velocity
-
-		if self.position.x > 1.1: self.position.x = -1.1
-		if self.position.x < -1.1: self.position.x = 1.1
-		if self.position.y > 1.1: self.position.y = -1.1
-		if self.position.y < -1.1: self.position.y = 1.1
+		self.screenWrap()
