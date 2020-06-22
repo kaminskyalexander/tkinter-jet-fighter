967bfa91a1bc4f06a0e33d858380d80d004e901a
Alexander <akcanadanet@gmail.com>
2020-06-22 13:45:24 -0400

    Seperated main loop from game class

---
+++ main.py
@@ -0,0 +1,38 @@
+from game import Game
+from interface import InterfaceManager, InterfaceStartup
+from setup import *
+
+ui = InterfaceManager()
+game = None
+
+def main():
+	"""
+	The main loop of the process.
+	Called every frame.
+	"""
+	global game, fullscreen
+	# Clear all objects from the screen
+	canvas.delete("all")
+
+	# Update input dictionary
+	inputs.refresh()
+
+	# Fullscreen
+	if inputs.key(*binds["f11"]):
+		fullscreen = not fullscreen
+		root.attributes("-fullscreen", fullscreen)
+
+	# Detect returns from the interface
+	response = ui.update()
+	if response != None:
+		# Start the game
+		game = Game(**response)
+
+	if game != None:
+		# Return to title screen after game completion
+		if game.update() == 0:
+			game = None
+			ui.currentInterface = InterfaceStartup()
+
+loop(main)
+tk.mainloop()
